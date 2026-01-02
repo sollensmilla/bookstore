ee026b07f02cd2ae78811685355a5aebc5a241a3
Smilla Sollén <ss226uk@student.lnu.se>
2026-01-02 17:41:45 +0100

    Basic routing and change type to module

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("index");
+});
+
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+
+router.get("/search", (req, res) => {
+    res.render("search");
+});
+
+export default router;
