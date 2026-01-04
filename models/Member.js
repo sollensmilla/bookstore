035b52b5ebfcf2b1924cc0ddfc28f73fb6474ea1
Smilla Sollén <ss226uk@student.lnu.se>
2026-01-04 13:25:56 +0100

    Model class for member

---
+++ models/Member.js
@@ -0,0 +1,59 @@
+import db from "../database/db.js";
+
+export default class Member {
+    constructor({
+        firstName,
+        lastName,
+        address,
+        city,
+        zip,
+        phone,
+        email,
+        password
+    }) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    findByEmail(email) {
+        return new Promise((resolve, reject) => {
+            const sql = "SELECT * FROM members WHERE email = ?";
+            db.query(sql, [email], (err, result) => {
+                if (err) return reject(err);
+                resolve(result[0] || null);
+            });
+        });
+    }
+
+    save() {
+        return new Promise((resolve, reject) => {
+            const sql = `
+        INSERT INTO members
+        (fname, lname, address, city, zip, phone, email, password)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+      `;
+
+            const values = [
+                this.firstName,
+                this.lastName,
+                this.address,
+                this.city,
+                this.zip,
+                this.phone,
+                this.email,
+                this.password
+            ];
+
+            db.query(sql, values, (err, result) => {
+                if (err) return reject(err);
+                resolve(result);
+            });
+        });
+    }
+}
