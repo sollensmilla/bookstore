0a3167d7e7bb1822ae3dfdea5b53f99571d1ec33
Smilla Sollén <ss226uk@student.lnu.se>
2026-01-09 18:33:12 +0100

    Move sensitive information to env file

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "NodeMySQL123!",
+    host: host,
+    user: user,
+    password: password,
     database: "book_store",
     waitForConnections: true,
     connectionLimit: 10
